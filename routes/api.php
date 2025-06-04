dbcb40c61fd53abdc411ad7bb2bc8b2b1bf502bc
Kacper <kgrzesik17@gmail.com>
2025-06-04 10:22:39 +0200

    set up the routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('events', EventController::class);
+
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);  // scope makes sure attendee are always a part or an event
