027586e7372eaeb824a34170fd6fa7bd4e58e2a0
Kacper <kgrzesik17@gmail.com>
2025-06-04 10:14:08 +0200

    basic class-table assignment for attendee, user and event model

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Models;
-
+// the Event and Attendee models are in the same namespace, so there is no need to import classes
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendee extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Models;
+// the Event and Attendee models are in the same namespace, so there is no need to import classes
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees(): HasMany
+    {
+        return $this->hasMany();
+    }
 }
