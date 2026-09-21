c632f68d10f4d6e07bdb066414e9b47d3db6d3bb
Kacper <kgrzesik17@gmail.com>
2026-09-21 18:39:58 +0200

    seeding data for the rest api

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use App\Models\Attendee;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
+    use HasFactory;
+
     public function User(): BelongsTo {
         return $this->belongsTo(User::class);
     }
